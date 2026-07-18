68f87391c50151c08f639a44051af2b223236ac4
Edson More Anton <edson20.15@hotmail.com>
2026-07-18 02:05:42 -0500

    debug: log PORT env var value

--- src/main/java/com/example/Proyecto_Reverdecer/ProyectoReverdecerApplication.java
+++ src/main/java/com/example/Proyecto_Reverdecer/ProyectoReverdecerApplication.java
@@ -8,8 +8,10 @@ public class ProyectoReverdecerApplication {
 
 	public static void main(String[] args) {
 		String port = System.getenv("PORT");
+		System.out.println("[PORT_DEBUG] PORT='" + port + "'");
 		if (port != null && !port.isEmpty()) {
 			System.setProperty("server.port", port);
+			System.out.println("[PORT_DEBUG] server.port -> " + port);
 		}
 		SpringApplication.run(ProyectoReverdecerApplication.class, args);
 	}
